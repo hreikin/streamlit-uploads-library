e0acb0f51f82e762cd5670783e51106ed95c3955
Michael Haslam <hreikin@gmail.com>
2023-03-07 23:16:07 +0000

    Create basic docstrings for class and methods.

--- src/streamlit_simple_gallery/__init__.py
+++ src/streamlit_simple_gallery/__init__.py
@@ -3,6 +3,32 @@ from pathlib import Path
 
 @st.cache_resource(show_spinner="Refreshing gallery...")
 class ImageGallery():
+    """Create a simple gallery out of streamlit widgets.
+
+    Using the gallery is simple, import `streamlit_simple_gallery` and then instantiate the class with the 
+    required `directory` variable. Other options can be configured by passing in different variables 
+    when instantiating the class.
+
+    Example Usage:
+        python
+        import streamlit as st
+        from streamlit_simple_gallery import ImageGallery
+
+        st.set_page_config(page_title="Streamlit Gallery", layout="wide")
+        default_gallery = ImageGallery(directory="assets")
+        gallery_with_columns = ImageGallery(directory="assets", label="**Gallery - Columns**", number_of_columns=3)
+        expander_gallery = ImageGallery(directory="assets", expanded=True, gallery_type="expander", label="**Gallery - Expander**")
+        multiple_options_gallery = ImageGallery(directory="assets", gallery_type="expander", label="**Gallery - Multiple Options**", number_of_columns=3, show_filename=False)
+   
+    Args:
+        directory (str): A str() of the path to the folder containing the gallery images, for example, "assets".
+        expanded (bool): A bool(), passing False starts the expander type gallery closed, default is open and True.
+        file_extensions (tuple): A tuple() containing strings of the file extensions to include in the gallery, default is (".png", ".jpg", ".jpeg").
+        gallery_type (str): A str() with either "container" or "expander" used as the keyword, the default is "container".
+        label (str or None): A str() containing the name of the gallery, passing None disables the label. The default value is "Gallery".
+        number_of_columns (int): An int() defining the number of required columns, default is 5.
+        show_filenames (bool): A bool(), passing True displays the filenames, the default is False which hides them.
+    """
     def __init__(self, directory, expanded=True, file_extensions=(".png", ".jpg", ".jpeg"), gallery_type="container", label="**Gallery**" or None, number_of_columns=5, show_filename=False):
         self.directory = Path(directory).resolve()
         self.expanded = expanded
@@ -14,6 +40,14 @@ class ImageGallery():
         self.gallery = self.create_gallery()
 
     def fetch_files(self):
+        """Returns a list of all files and filenames.
+
+        Returns a list of files and a list of filenames to be used by create_gallery().
+        
+        Returns:
+            all_files (list): A list of files.
+            all_filenames (list): A list of filenames.
+        """
         self.all_files = list()
         self.all_filenames = list()
         for item in self.directory.rglob("*"):
@@ -23,6 +57,13 @@ class ImageGallery():
         return self.all_files, self.all_filenames
 
     def create_gallery(self):
+        """Creates a simple gallery with columns.
+
+        Creates a gallery using columns out of streamlit widgets.
+        
+        Returns:
+            container_or_expander (st.container or st.expander): A streamlit widget containing the gallery.
+        """
         if self.gallery_type == "expander":
             if self.label == None:
                 self.label = ""
