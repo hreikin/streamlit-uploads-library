7845ac840af0fe5f2834a81d2d497f53e6353e84
Michael Haslam <hreikin@gmail.com>
2023-03-07 18:54:03 +0000

    Dynamically generate the number of columns from the supplied value, defaults to 5.

--- src/streamlit_simple_gallery/__init__.py
+++ src/streamlit_simple_gallery/__init__.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 @st.cache_resource(show_spinner="Refreshing gallery...")
 class ImageGallery():
-    def __init__(self, directory, expanded=True, file_extensions=(".png", ".jpg", ".jpeg"), gallery_type="container", label="**Gallery**"):
+    def __init__(self, directory, expanded=True, file_extensions=(".png", ".jpg", ".jpeg"), gallery_type="container", label="**Gallery**", number_of_columns=5):
         self.directory = Path(directory).resolve()
         self.expanded = expanded
         self.file_extensions = file_extensions
         self.gallery_type = gallery_type
         self.label = label
+        self.number_of_columns = number_of_columns
         self.gallery = self.create_gallery()
 
     def fetch_files(self):
@@ -28,15 +29,17 @@ class ImageGallery():
             with self.source_image_dropdown_container:
                 self.gallery_label = st.markdown(f"**{self.label}**")
         with self.source_image_dropdown_container:
-            self.col1, self.col2, self.col3, self.col4, self.col5 = st.columns(5)
-            self.col_list = [self.col1, self.col2, self.col3, self.col4, self.col5]
             self.col_idx = 0
             self.filename_idx = 0
+            self.max_idx = self.number_of_columns-1
             self.gallery_files, self.gallery_filenames = self.fetch_files()
+            self.all_columns = list(st.columns(self.number_of_columns))
+            for item in self.all_columns:
+                print(item)
             for img in self.gallery_files:
-                with self.col_list[self.col_idx]:
+                with self.all_columns[self.col_idx]:
                     st.image(img, caption=self.gallery_filenames[self.filename_idx], use_column_width=True)
-                    if self.col_idx < 4:
+                    if self.col_idx < self.max_idx:
                         self.col_idx += 1
                     else:
                         self.col_idx = 0
