f28941b662dac609f832062c4c28d16ef0dae011
Michael Haslam <hreikin@gmail.com>
2023-03-10 21:58:10 +0000

    Update sub class with new required variables.

--- src/streamlit_uploads_library/gallery.py
+++ src/streamlit_uploads_library/gallery.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from src.streamlit_uploads_library.library import Library
 
 class Gallery(Library):
-    def __init__(self, directory, file_extensions=(".png", ".jpg", ".jpeg"), number_of_columns=5, show_details=False):
-        super(Gallery, self).__init__(directory, file_extensions, number_of_columns, show_details)
+    def __init__(self, directory, expanded=False, file_extensions=(".png", ".jpg", ".jpeg"), number_of_columns=5, show_details=False):
+        super(Gallery, self).__init__(directory, expanded, file_extensions, number_of_columns, show_details)
 
     def fetch_files(self):
         return super().fetch_files()
