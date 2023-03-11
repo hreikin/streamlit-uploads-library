bc3253a4ec1568033cb6e8083815a2466fc3d8e9
Michael Haslam <hreikin@gmail.com>
2023-03-11 23:30:22 +0000

    Remove unused parameters from class.

--- src/streamlit_uploads_library/gallery.py
+++ src/streamlit_uploads_library/gallery.py
@@ -2,8 +2,8 @@ import streamlit as st
 from src.streamlit_uploads_library.library import Library
 
 class Gallery(Library):
-    def __init__(self, directory, expanded=False, file_extensions=(".png", ".jpg", ".jpeg"), number_of_columns=5, show_details=False):
-        super(Gallery, self).__init__(directory, expanded, file_extensions, number_of_columns, show_details)
+    def __init__(self, directory, file_extensions=(".png", ".jpg", ".jpeg"), number_of_columns=5):
+        super(Gallery, self).__init__(directory, file_extensions, number_of_columns)
 
     def fetch_files(self):
         return super().fetch_files()
